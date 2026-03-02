8d0c506adc8b66325326cb57abfa3da0a168da48
ChrisantLena <chrisant.ardiyanti.lena-2023@fst.unair.ac.id>
2026-03-02 15:15:20 +0700

    authentication

---
+++ SistemSAAS/app/Http/Middleware/HandleCors.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $allowedOrigins = [
+            'http://localhost:5173',
+            'http://127.0.0.1:5173',
+            'http://localhost:3000',
+        ];
+
+        $origin = $request->header('Origin');
+
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('', 204);
+        } else {
+            $response = $next($request);
+        }
+
+        if (in_array($origin, $allowedOrigins)) {
+            $response->headers->set('Access-Control-Allow-Origin', $origin);
+        }
+
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
